81a45ef2d8ce2f76b98de348b1fee98ac57c471f
Ajay <ajay.cherot@nkorrtech.com>
2023-09-04 09:59:48 +0530

    Test Project Created

---
+++ Test_Project/src/testpkg/HelloWorld.java
@@ -0,0 +1,58 @@
+package testpkg;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class HelloWorld {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HelloWorld window = new HelloWorld();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public HelloWorld() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnClicked = new JButton("Click Here");
+		btnClicked.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				JOptionPane.showMessageDialog(btnClicked, "Hello World");
+			}
+		});
+		btnClicked.setBounds(162, 110, 105, 29);
+		frame.getContentPane().add(btnClicked);
+	}
+}
